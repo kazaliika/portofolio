cf77c07cd2389031cc52792bc6b3116dcff925ad
kazaliika <mavero811@gmail.com>
2024-05-15 21:13:49 +0700

    add animasi container pada file add story screen

--- lib/ui/add_story_screen.dart
+++ lib/ui/add_story_screen.dart
@@ -1,13 +1,63 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class AddStoryScreen extends StatelessWidget {
+double randomBorderRadius() {
+  return Random().nextDouble() * 20;
+}
+
+double randomMargin() {
+  return Random().nextDouble() * 20;
+}
+
+class AddStoryScreen extends StatefulWidget {
   const AddStoryScreen({super.key});
+
+  @override
+  State<AddStoryScreen> createState() => _AddStoryScreenState();
+}
+
+class _AddStoryScreenState extends State<AddStoryScreen> {
+  late double borderRadius;
+  late double margin;
+
+  void initState() {
+    borderRadius = randomBorderRadius();
+    margin = randomMargin();
+  }
+
+  void changeState() {
+    setState(() {
+      borderRadius = randomBorderRadius();
+      margin = randomMargin();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Hero(tag: 'image_animation', child: Image.asset('assets/IMG.jpg')),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 2000),
+                  curve: Curves.bounceIn,
+                  margin: EdgeInsets.all(margin),
+                  decoration: BoxDecoration(
+                      color: Colors.cyanAccent,
+                      borderRadius: BorderRadius.circular(borderRadius)),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () => changeState(), child: Text('ubah'))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
