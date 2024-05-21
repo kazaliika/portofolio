3437b2f7e0d9b908444a569e928e9a6395953f41
kazaliika <mavero811@gmail.com>
2024-05-21 20:24:01 +0700

    class exercises

--- lib/ui/info_detail_screen.dart
+++ lib/ui/info_detail_screen.dart
@@ -1,65 +1,127 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class InfoDetailScreen extends StatefulWidget {
+  const InfoDetailScreen({super.key});
+
   @override
-  _InfoDetailScreenState createState() => _InfoDetailScreenState();
+  State<InfoDetailScreen> createState() => _InfoDetailScreenState();
 }
 
 class _InfoDetailScreenState extends State<InfoDetailScreen> {
-  Color containerColor = Colors.blue;
-  Offset pos = Offset(100, 100);
-  Random random = Random();
+  final double boxSize = 150.0;
+  int numOfSingleTap = 0;
+  int numOfDoubleTap = 0;
+  int numOfLongPress = 0;
 
-  late double borderRadius = 0;
-  late EdgeInsets margin = EdgeInsets.all(0);
+  late double borderRadius;
+  late double margin;
+  late Color warna;
 
+  double posX = 0.0;
+  double posY = 0.0;
+
+  @override
   void initState() {
-    // super.initState();
-    setState(() {
-      containerColor = Color.fromARGB(
-        255,
-        random.nextInt(256),
-        random.nextInt(256),
-        random.nextInt(256),
-      );
-      borderRadius = random.nextDouble() * 50;
-      margin = EdgeInsets.all(random.nextDouble() * 100);
-    });
+    super.initState();
+    margin = randomMargin();
+    borderRadius = randomBorderRadius();
+    warna = randomCalor();
   }
 
-  void changeState(DragUpdateDetails details) {
+  double randomBorderRadius() {
+    return Random().nextDouble() * 100;
+  }
+
+  double randomMargin() {
+    return Random().nextDouble() * 20;
+  }
+
+  Color randomCalor() {
+    return Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+  }
+
+  void changeState() {
     setState(() {
-      pos += details.delta;
+      borderRadius = randomBorderRadius();
+      margin = randomMargin();
+      warna = randomCalor();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: initState,
-      onPanUpdate: changeState,
-      child: Container(
-        color: Colors.white,
-        child: Stack(
-          children: [
-            Positioned(
-              left: pos.dx,
-              top: pos.dy,
+    print(borderRadius);
+    if (posX == 0) {
+      CenterPosition(context);
+    }
+
+    return Scaffold(
+      body: Stack(children: [
+        Positioned(
+          left: posX,
+          top: posY,
+          child: GestureDetector(
+            onPanUpdate: (DragUpdateDetails details) {
+              setState(() {
+                double verticalPosition = details.delta.dy;
+                double horizontalPosition = details.delta.dx;
+                posX += horizontalPosition;
+                posY += verticalPosition;
+              });
+            },
+            onTap: () {
+              setState(() {
+                numOfSingleTap++;
+                changeState();
+              });
+            },
+            onDoubleTap: () {
+              setState(() {
+                numOfDoubleTap++;
+              });
+            },
+            onLongPress: () {
+              setState(() {
+                numOfLongPress++;
+              });
+            },
+            child: SizedBox(
+              width: 200,
+              height: 200,
               child: AnimatedContainer(
-                duration: Duration(milliseconds: 100),
-                width: 100,
-                height: 100,
-                margin: margin,
+                duration: Duration(milliseconds: 1000),
+                curve: Curves.easeIn,
+                margin: EdgeInsets.all(margin),
                 decoration: BoxDecoration(
-                  color: containerColor,
+                  color: warna,
                   borderRadius: BorderRadius.circular(borderRadius),
                 ),
               ),
             ),
-          ],
+          ),
         ),
+      ]),
+      bottomNavigationBar: Container(
+        color: Color.fromARGB(255, 255, 255, 255),
+        height: 60,
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          Text("Taps : $numOfSingleTap"),
+          Text("Double Taps : $numOfDoubleTap"),
+          Text("Long Press : $numOfLongPress")
+        ]),
       ),
     );
+  }
+
+  void CenterPosition(BuildContext context) {
+    posX = (MediaQuery.of(context).size.width / 2) - boxSize / 2;
+    posY = (MediaQuery.of(context).size.height / 2) - boxSize / 2;
+
+    setState(() {
+      this.posX = posX;
+      this.posY = posY;
+    });
   }
 }
