91ec7831f1bacde3308bab70370e2707629ae95b
kazaliika <mavero811@gmail.com>
2024-05-25 10:00:10 +0700

    add assets

--- lib/ui/add_story_screen.dart
+++ lib/ui/add_story_screen.dart
@@ -1,14 +1,4 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-
-double randomBorderRadius() {
-  return Random().nextDouble() * 20;
-}
-
-double randomMargin() {
-  return Random().nextDouble() * 20;
-}
 
 class AddStoryScreen extends StatefulWidget {
   const AddStoryScreen({super.key});
@@ -18,45 +8,13 @@ class AddStoryScreen extends StatefulWidget {
 }
 
 class _AddStoryScreenState extends State<AddStoryScreen> {
-  late double borderRadius;
-  late double margin;
-
-  void initState() {
-    super.initState();
-    borderRadius = randomBorderRadius();
-    margin = randomMargin();
-  }
-
-  void changeState() {
-    setState(() {
-      borderRadius = randomBorderRadius();
-      margin = randomMargin();
-    });
-  }
-
+  bool isHover = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                width: 100,
-                height: 100,
-                child: AnimatedContainer(
-                  duration: Duration(milliseconds: 2000),
-                  curve: Curves.bounceIn,
-                  margin: EdgeInsets.all(margin),
-                  decoration: BoxDecoration(
-                      color: Colors.cyanAccent,
-                      borderRadius: BorderRadius.circular(borderRadius)),
-                ),
-              ),
-              ElevatedButton(
-                  onPressed: () => changeState(), child: Text('ubah'))
-            ],
-          ),
+          child: Image.network('https://picsum.photos/200/300'),
         ),
       ),
     );
