06abe9b61c3b1681d668b94cdf76ca5961d9d9cf
kazaliika <mavero811@gmail.com>
2024-05-26 13:33:21 +0700

    fix hover & gesturedetector

--- lib/ui/add_story_screen.dart
+++ lib/ui/add_story_screen.dart
@@ -8,14 +8,42 @@ class AddStoryScreen extends StatefulWidget {
 }
 
 class _AddStoryScreenState extends State<AddStoryScreen> {
-  bool isHover = false;
+  double _scale = 1.0;
+
+  void _onHover(PointerEvent details) {
+    setState(() {
+      _scale = 1.2;
+    });
+  }
+
+  void _onExit(PointerEvent details) {
+    setState(() {
+      _scale = 1.0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Container(
-            child: Image.network('https://picsum.photos/250?image=9'),
+      body: Center(
+        child: MouseRegion(
+          onHover: _onHover,
+          onExit: _onExit,
+          child: GestureDetector(
+            onTap: () {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('Gambar disentuh!'),
+              ));
+            },
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 50),
+              transform: Matrix4.identity()..scale(_scale),
+              child: Image.network(
+                'https://picsum.photos/id/200/300',
+                width: 300,
+                height: 300,
+              ),
+            ),
           ),
         ),
       ),
